9dd3822004a4bd5179079005ab1ce3c357edf62b
Jhon <malaquiasjonatham@gmail.com>
2022-01-29 12:45:09 -0300

    botão registro2

--- script/script.js
+++ script/script.js
@@ -1,12 +1,7 @@
 const button = document.querySelector(".btn");
 
 function clicar(e) {
-    e.preventDefault();
-
-    if(button.className == "btn"){
-        button.className = "btn clicado";
-    }
-
+    button.className = "btn clicado";
 }
 
 button.addEventListener('click', clicar);
