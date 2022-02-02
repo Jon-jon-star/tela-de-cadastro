0cd3917592b86337587cc17c3a2bddfa810c9c06
Jhon <malaquiasjonatham@gmail.com>
2022-02-02 11:55:52 -0300

    inplementando javascript

--- script/script.js
+++ script/script.js
@@ -1,7 +1,18 @@
-const button = document.querySelector(".btn");
+const containerLogin = document.querySelector(".container");
+const cadastroContainer = document.querySelector(".cadastro-Container");
+const criarConta = document.querySelector("#semConta");
+const btnVoltar = document.querySelector("#back");
 
-function clicar(e) {
-    button.className = "btn clicado";
+function fazerCadastro() {
+    containerLogin.setAttribute("style", "display: none")
+    cadastroContainer.setAttribute("style", "display: block")
 }
 
-button.addEventListener('click', clicar);
+criarConta.addEventListener('click', fazerCadastro);
+
+function voltarTela(){
+    containerLogin.setAttribute("style", "display: block");
+    cadastroContainer.setAttribute("style", "display: none")
+}
+
+btnVoltar.addEventListener('click', voltarTela);
